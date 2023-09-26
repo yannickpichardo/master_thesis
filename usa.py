f31dc2f83b493501956190ff92b9960f3e20c705
Yannick <yannickpichardo@gmail.com>
2023-09-26 16:21:57 +0200

    Added Hurricane Data and HPI

--- usa.py
+++ usa.py
@@ -1,5 +1,6 @@
 # Create a reverse mapping from abbreviation to full names
-state_dict = {
+def state_name_to_abbreviation(state_name):
+    state_dict_2 = {
     'Alabama': 'AL', 'Alaska': 'AK', 'American Samoa': 'AS', 'Arizona': 'AZ', 
     'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO', 'Connecticut': 'CT', 
     'Delaware': 'DE', 'District of Columbia': 'DC', 'Florida': 'FL', 'Georgia': 'GA', 
@@ -15,15 +16,29 @@ state_dict = {
     'Utah': 'UT', 'Vermont': 'VT', 'Virgin Islands': 'VI', 'Virginia': 'VA', 
     'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'
     }
+    for name, abbreviation in state_dict_2.items():
+        if name == state_name:
+            return abbreviation
+    return None
 
 def state_abbreviation_to_name(abbr):
+    state_dict = {
+    'Alabama': 'AL', 'Alaska': 'AK', 'American Samoa': 'AS', 'Arizona': 'AZ', 
+    'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO', 'Connecticut': 'CT', 
+    'Delaware': 'DE', 'District of Columbia': 'DC', 'Florida': 'FL', 'Georgia': 'GA', 
+    'Guam': 'GU', 'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': 'IL', 'Indiana': 'IN', 
+    'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME', 
+    'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 
+    'Mississippi': 'MS', 'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 
+    'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ', 'New Mexico': 'NM', 
+    'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 
+    'Northern Mariana Islands': 'MP', 'Ohio': 'OH', 'Oklahoma': 'OK', 
+    'Oregon': 'OR', 'Pennsylvania': 'PA', 'Puerto Rico': 'PR', 'Rhode Island': 'RI', 
+    'South Carolina': 'SC', 'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 
+    'Utah': 'UT', 'Vermont': 'VT', 'Virgin Islands': 'VI', 'Virginia': 'VA', 
+    'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'
+    }
     for name, abbreviation in state_dict.items():
         if abbreviation == abbr:
             return name
-    return None
-
-def state_name_to_abbreviation(state_name):
-    for name, abbreviation in state_dict.items():
-        if name == state_name:
-            return abbreviation
     return None
